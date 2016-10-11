81cf0049ca81c732cd47ee95e6f6b778c05384af
lenovo\lenovo-pc <2213286021@qq.com>
2016-10-11 15:09:02 +0900

    增加了一些素材，调整缓动动画，修正了不是两个页面的错误

--- PersonalProfile/src/Scroll.ts
+++ PersonalProfile/src/Scroll.ts
@@ -27,6 +27,43 @@ class Scroll extends egret.DisplayObjectContainer {
         //stage.addChild(this.logo);
         var bg:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/bg_scroll.jpg");
         var bg2:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/bg_scroll2.jpg");
+        var bubble1:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/bubble1.png");
+        bubble1.alpha = 0;
+        bubble1.x = -640;
+        bubble1.y = 300;
+        var bubble2:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/bubble2.png");
+        bubble2.alpha = 0;
+        bubble2.x = -640;
+        bubble2.y = 450;
+        var bubble3:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/bubble3.png");
+        bubble3.alpha = 0;
+        bubble3.x = -640;
+        bubble3.y = 600;
+        var title:egret.gui.UIAsset = new egret.gui.UIAsset("resource/assets/title.png");
+        title.y = 100;
+
+        var rect:egret.gui.Rect = new egret.gui.Rect();
+        rect.fillAlpha = 0.5;
+        rect.fillColor = 0x000000;
+        rect.width = 640;
+        rect.height = 200;
+        rect.y = 100;
+
+        var endText:egret.TextField = new egret.TextField();
+        endText.text = "喵喵喵喵喵喵"
+        endText.size = 32;
+        endText.y = 500;
+        this.addChild(endText);
+
+        var mask:egret.Shape = new egret.Shape();
+        mask.graphics.lineStyle( 0x000000 )
+        mask.graphics.beginFill(0xffffff);
+        mask.graphics.drawRect(0,0,640,100);
+        mask.graphics.endFill();
+        mask.y = 900;
+        this.addChild(mask);
+        mask.mask = endText;
+
         var text:egret.gui.TextBase = new egret.gui.TextBase();
         text.text = "滑动观看下一页\n\n          ﹀";
         text.size = 32;
@@ -60,7 +97,7 @@ class Scroll extends egret.DisplayObjectContainer {
         text_intro.anchorOffsetY = text_intro.height/2;
         text_intro.fontFamily = "微软雅黑";
         text_intro.x = 0;
-        text_intro.y = 140;
+        text_intro.y = 180;
         text_intro.height = 100;
         text_intro.width = 640;
         text_intro.alpha = 0;
@@ -80,12 +117,17 @@ class Scroll extends egret.DisplayObjectContainer {
         var group:egret.gui.Group = new egret.gui.Group();
         group.addElement(bg);
         group.addElement(text);
+        group.addElement(rect);
+        group.addElement(title);
         stage.addChild(group);
 
         var group2:egret.gui.Group = new egret.gui.Group();
         group2.addElement(bg2);
         group2.addElement(text_title);
         group2.addElement(text_intro);
+        group2.addElement(bubble1);
+        group2.addElement(bubble2);
+        group2.addElement(bubble3);
         group2.y = 1136;
         stage.addChild(group2);
         
@@ -120,6 +162,18 @@ class Scroll extends egret.DisplayObjectContainer {
                     tw.wait(1200);
                     tw.to({"alpha":1},250,egret.Ease.sineInOut);
                     tw.to({"size":26},250,egret.Ease.sineInOut);
+                    tw = egret.Tween.get(bubble1);
+                    tw.wait(1700);
+                    tw.to({"alpha":1},300,egret.Ease.backOut);
+                    tw.to({"x":0},300,egret.Ease.backOut);
+                    tw = egret.Tween.get(bubble2);
+                    tw.wait(2200);
+                    tw.to({"alpha":1},300,egret.Ease.backOut);
+                    tw.to({"x":0},300,egret.Ease.backOut);
+                    tw = egret.Tween.get(bubble3);
+                    tw.wait(2700);
+                    tw.to({"alpha":1},300,egret.Ease.backOut);
+                    tw.to({"x":0},300,egret.Ease.backOut);
                 }else{
                     text_title.visible = false;
                     text_intro.visible = false;
@@ -143,7 +197,15 @@ class Scroll extends egret.DisplayObjectContainer {
             if(beginPoint.y-endPoint.y<=-100 && currentPage > 1){
                 egret.Tween.get(group).to({x:0,y:1136*(currentPage-2)},1000,egret.Ease.sineInOut);
                 egret.Tween.get(group2).to({x:0,y:1136*(currentPage-1)},1000,egret.Ease.sineInOut);
-                currentPage--;        
+                currentPage--;    
+                egret.Tween.get(text_title).to({"alpha":0},200,egret.Ease.sineIn);   
+                egret.Tween.get(text_intro).to({"size":0},200,egret.Ease.sineIn);   
+                egret.Tween.get(bubble1).to({"alpha":0},300,egret.Ease.backIn);   
+                egret.Tween.get(bubble1).to({"x":-640},300,egret.Ease.backIn);   
+                egret.Tween.get(bubble2).to({"alpha":0},300,egret.Ease.backIn);   
+                egret.Tween.get(bubble2).to({"x":-640},300,egret.Ease.backIn);   
+                egret.Tween.get(bubble3).to({"alpha":0},300,egret.Ease.backIn);   
+                egret.Tween.get(bubble3).to({"x":-640},300,egret.Ease.backIn);   
             }
         },this);      
         
